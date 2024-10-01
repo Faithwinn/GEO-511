325db9522fbd1eaab82b5ef2b4906675ea564362
Gbadamosi <figbadam@buffalo.edu>
2024-10-01 15:19:20 -0400

    Second script for case study 5

--- Case study 5.R
+++ Case study 5.R
@@ -40,9 +40,67 @@ ggplot() +
   geom_sf(data = ny_buffer, fill = "red", alpha = 0.5) +
   theme_minimal() +
   ggtitle("New York Land within 10km of Canadian Border",
-          subtitle = paste0("Area to defend: ", area_km2, " km²"))
+          subtitle = paste0("Area to defend: 3514.19 km²"))
 
-# Save the result if needed
-# st_write(ny_buffer, "ny_canada_buffer.gpkg")
 
-print(paste("Area to defend from Canadians:", area_km2, "square kilometers"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Second Try 
+#Load datasets
+
+data("world")
+data("us_states")
+
+#Create Albers for transformation 
+albers <- "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"
+
+# Transform data to Albers projection
+world_albers <- st_transform(world, albers)  #start from world since we are using world 
+states_albers <- st_transform(us_states, albers)
+
+#Now transform the data to albers 
+canada <- world_albers%>% #easier to remove from the projected dataset to avoid error in geos_op2_geom
+  filter(name_long == "Canada")
+
+# Create 10km buffer around Canadian border
+canada_buffer <- st_buffer(canada, dist = 10000)
+
+
+#Filter Us_states to remain only New York 
+ny <- states_albers %>% #easier to remove from the projected dataset to avoid error in geos_op2_geom
+  filter(NAME == 'New York')
+
+#Intersect buffer with NY to get land within 10km of border
+ny_buffer <- st_intersection(ny, canada_buffer)
+
+#(finally escaped the geo problem)
+
+ggplot() + 
+  geom_sf(data = ny, fill = "lightgray") +
+  geom_sf(data = canada_buffer, fill = "red", alpha = 0.5) +
+  theme_minimal() +
+  ggtitle("New York Land within 10km of Canadian Border", subtitle = "Area to defend: 3495.19 km²")
+  
+  
+  #calculate area 
+  area_km2 <- st_area(ny_buffer) %>%
+  set_units(km^2) %>%
+  round(2)
+  
+  area_km2
+  
